9d96e227fe89aaa70c094c0a4fae326c987b02f0
ArtOS Developer <61917581+ArtOS-Dev@users.noreply.github.com>
2020-03-15 05:35:52 +0700

    Fix Derp : Checkra1n

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ LOCAL_PATH := vendor/checkra1n
 
 # Checkra1n 
 PRODUCT_COPY_FILES += \
-    vendor/checkra1n/checkra1n:$(TARGET_COPY_OUT_SYSTEM)/bin/checkra1n
+    vendor/checkra1n/checkra1n/checkra1n:$(TARGET_COPY_OUT_SYSTEM)/bin/checkra1n
